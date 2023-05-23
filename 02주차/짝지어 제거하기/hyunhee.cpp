340349bbbfeb547811e13f7b2fbba7f1bbc976d7
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-05-23 14:26:32 +0900

    Update hyunhee.cpp
    처음 풀이 방법은 vector로 temp를 만들어서 완전히 s와 같게 만든 뒤
    while문으로 계속 돌면서 같은 값이 나올때마다 erase로 지워주었다.
    이렇게 했을 때, erase함수를 쓸 때마다 vector의 이동이 생기므로 효율성 측면에서 계속 0점이 나옴
    찾아본 결과, 한 요소를 담을 때마다 삭제하여 결론적으로 대이동이 필요 없는 stack을 사용
    매번 top에 해당하는 아이와 새로 비교할 s의 index를 비교하여
    같은것이 나왔을 경우 pop을, 아닐 경우 push를 해주고
    마지막에 stack에 아무것도 남지 않게 되었을 때는 answer를 1로 바꿔준다.

--- 02주차/짝지어 제거하기/hyunhee.cpp
+++ 02주차/짝지어 제거하기/hyunhee.cpp
@@ -1,39 +1,24 @@
 #include <iostream>
 #include<string>
-#include <vector>
+#include <stack>
 using namespace std;
 
 int solution(string s)
 {
-    int answer = -1;
-    vector <char> temp;
-    for (int i = 0; i< s.length(); i++)
-        temp.push_back(s[i]); //temp 구현
-    while(1)
+    int answer = 0;
+    stack<char> temp;
+    if(temp.size() == 1)
+        answer = 0;
+    for (int i = 0; i < s.length(); i++)
     {
-        if(temp.size() == 1)
-            return 0;
-        else if(temp.size() == 2)
-            if(temp[0] == temp[1])
-                return 1;
-            else
-                return 0;
+        if(temp.size() == 0)
+            temp.push(s[i]);
+        else if(temp.top() == s[i])
+            temp.pop();
         else
-        {
-            int stat = 0;
-            for(int i = 0; i < temp.size() - 1;i++)
-            {
-                if(temp[i] == temp[i + 1])
-                {
-                    temp.erase(temp.begin() + i);
-                    temp.erase(temp.begin() + i);
-                    stat = 1;
-                    break;
-                }
-            }
-            if(stat == 0)
-                return 0;
-        }
+            temp.push(s[i]);
     }
+    if(temp.size() == 0)
+        answer = 1;
     return answer;
 }
