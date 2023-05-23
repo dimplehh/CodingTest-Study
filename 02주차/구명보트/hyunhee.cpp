a1970007a6ce36d5d38fe851103e8b35609ed6a1
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-05-23 15:33:58 +0900

    Update hyunhee.cpp
    그리디 알고리즘 문제.
    그리디인 것을 아는 방법? 동전 최소로 써서 계산하는 방법같은 문제와 비슷할 때 떠올릴 수 있음
    포인트는 '개수'를 구하는 것만이 목적.
    문제에서 중요한 조건 = 최대 두명만 태울 수 있음 == 최댓값과 최솟값의 합과 리밋을 비교할 것
    우선, 오름차순으로 정렬을 한 뒤에 vector의 끝값을 나타내는 back + 벡터 최솟값과 리밋을 비교함
    vector의 앞을 삭제하는것은 불가능하거나 너무 시간이 오래걸림, 우리는 개수를 구하는것만이 목적이므로
    index를 옮기는 방법을 택해서 result를 구하기로 함.
    최솟값과 최댓값이 limit보다 작거나 같을 때만 최솟값은 빠졌다고 치고 index를 증가시킬 수 있고
    그렇지 않으면 그냥 최댓값 뺐다는 의미로 result만 증가시키기로 함.
    이렇게 index와 pop되어 남은 people의 size가 같아지면 while문을 빠져나옴

--- 02주차/구명보트/hyunhee.cpp
+++ 02주차/구명보트/hyunhee.cpp
@@ -1,1 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int result = 0;
+    sort(people.begin(), people.end());
+    int i = 0;
+    while(i< people.size())
+    {
+        if(people[i] + people.back() <= limit)
+            i++;
+        result++;
+        people.pop_back();
+    }
+    return result;
+}
