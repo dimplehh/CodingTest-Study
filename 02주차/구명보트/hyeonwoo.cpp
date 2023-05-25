7b1d81852601c98a8473e2527d260625783fd7a8
jeongHyeonu <114675420+jeongHyeonu@users.noreply.github.com>
2023-05-25 22:04:51 +0900

    Fix : 문제 풀이 필요없는부분 제거
    .

--- 02주차/구명보트/hyeonwoo.cpp
+++ 02주차/구명보트/hyeonwoo.cpp
@@ -3,13 +3,6 @@
 #include <algorithm>
 
 using namespace std;
-int pickedPeople[50000] = { 0, };
-
-bool isPickedAll(int peopleCnt) {
-    for (int i = 0; i < peopleCnt; i++) {
-        if (pickedPeople[i] == 0) return false;
-    }
-}
 
 int solution(vector<int> people, int limit) {
     int answer = 0, idx=0, peopleCnt=0, target;
