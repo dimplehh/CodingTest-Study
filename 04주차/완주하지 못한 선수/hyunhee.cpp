56f03f6c40efed916223d30969aa8601050d7397
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-06-04 17:38:14 +0900

    Update hyunhee.cpp
    sort 이용한 풀이

--- 04주차/완주하지 못한 선수/hyunhee.cpp
+++ 04주차/완주하지 못한 선수/hyunhee.cpp
@@ -1,1 +1,24 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    answer = participant.back();
+    for (int i = 0; i < completion.size(); i++)
+    {
+        if(participant[i] != completion[i])
+        {
+            answer = participant[i];
+            break;
+        }
+    }
+    return answer;
+}
 
