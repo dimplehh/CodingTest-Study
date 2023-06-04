5cb745427fec452b48bb0d0e3f064848787ffd62
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-06-04 19:55:25 +0900

    Update hyunhee.cpp
    너비 우선 탐색(BFS)로 풀어야만 시간 초과가 발생하지 않는 문제

--- 04주차/숫자 변환하기/hyunhee.cpp
+++ 04주차/숫자 변환하기/hyunhee.cpp
@@ -1,8 +1,63 @@
 #include <string>
 #include <vector>
+#include <queue>
+#include <set>
+using namespace std;
+
+int solution(int x, int y, int n)
+{
+	if (x == y)
+		return 0;
+
+	int answer{ 0 };
+
+	// 숫자, Lv(== 횟수)
+	queue<pair<int,int>> queue;
+	set<int> set;
+
+	set.insert(x);
+	queue.push({x,0});
+
+	while (!queue.empty())
+	{
+		auto data = queue.front();
+		queue.pop();
+
+		if (data.first == y)
+		{
+			answer = data.second;
+			break;
+		}
+		else if (data.first < y)
+		{
+			int X2 = data.first * 2;
+			if (set.insert(X2).second)
+			{
+				queue.push({ data.first * 2,data.second + 1 });
+			}
+
+			int X3 = data.first * 3;
+			if(set.insert(X3).second)
+				queue.push({ data.first * 3,data.second + 1 });
+			
+			int AddN = data.first + n;
+			if(set.insert(AddN).second)
+				queue.push({ data.first + n,data.second + 1 });
+		}
+	}
+
+	if (answer == 0)
+		return -1;
+
+	return answer;
+}
+
+
+/*#include <string>
+#include <vector>
 #include <iostream>
 
-using namespace std;
+using namespace std; 
 int answer; 
 void DFS(int x, int y, int n, int count){
     if(x == y)
@@ -28,4 +83,4 @@ int solution(int x, int y, int n) {
     if(x != y && answer == 0)
         answer = -1;
     return answer;
-}
+}*/
