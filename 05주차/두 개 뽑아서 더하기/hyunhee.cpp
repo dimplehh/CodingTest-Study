4cafc209d19b8f43e7c115c918902998b4526a9f
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-06-11 16:17:36 +0900

    Update hyunhee.cpp
    set을 활용하면 자동으로 중복 제거가 되고 이후에 assign을 활용하여 set -> vector로 바꿀 수 있음.
    그렇지 않은 경우에는 sort 후 erase함수를 이용하여 중복 제거 가능!

--- 05주차/두 개 뽑아서 더하기/hyunhee.cpp
+++ 05주차/두 개 뽑아서 더하기/hyunhee.cpp
@@ -1,1 +1,39 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
 
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    sort(numbers.begin(), numbers.end());
+    for(int i=0; i< numbers.size();i++)
+    {
+        for(int j = i + 1; j < numbers.size(); j++)
+        {
+            answer.push_back(numbers[i] + numbers[j]);
+        }
+    }
+    sort(answer.begin(), answer.end());
+    answer.erase(unique(answer.begin(), answer.end()), answer.end());
+    return answer;
+}
+
+
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// #include <set>
+// using namespace std;
+
+// vector<int> solution(vector<int> numbers) {
+//     vector<int> answer;
+//     set<int> st;
+//     for(int i = 0;i<numbers.size();++i){
+//         for(int j = i+1 ; j< numbers.size();++j){
+//             st.insert(numbers[i] + numbers[j]);
+//         }
+//     }
+//     answer.assign(st.begin(), st.end());
+//     return answer;
+// }
