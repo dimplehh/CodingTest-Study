ceb402f229cd4691dac278d8ce6f16c09b896909
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-06-23 19:30:32 +0900

    Update hyunhee.cpp

--- 07주차/피로도/hyunhee.cpp
+++ 07주차/피로도/hyunhee.cpp
@@ -1,1 +1,22 @@
-
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+bool visited[8] = {false,};
+int answer = 0;
+void DFS(int k, vector<vector<int>> dungeons, int cnt)
+{
+    answer = max(answer, cnt);
+    for (int i=0; i<dungeons.size(); i++) {
+        if (!visited[i] && dungeons[i][0] <= k) {
+            visited[i] = true;
+            DFS(k - dungeons[i][1], dungeons, cnt + 1);
+            visited[i] = false;
+        }
+    }
+}
+int solution(int k, vector<vector<int>> dungeons) {
+    DFS(k, dungeons, 0);
+    return answer;
+}
