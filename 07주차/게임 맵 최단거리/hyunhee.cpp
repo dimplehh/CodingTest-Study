eda9b0651f7f5fe7e6bc4e6fab99c1b6e3643a01
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-06-27 15:23:10 +0900

    Update hyunhee.cpp

--- 07주차/게임 맵 최단거리/hyunhee.cpp
+++ 07주차/게임 맵 최단거리/hyunhee.cpp
@@ -32,3 +32,46 @@ int solution(vector<vector<int> > maps)
         answer = -1;
     return answer;
 }
+
+//두번째 시도 : BFS -> 통과
+#include<vector>
+#include<iostream>
+#include<queue>
+using namespace std;
+bool visited[101][101];
+int answer = 10001;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+int solution(vector<vector<int> > maps)
+{
+    queue <pair<int, pair<int, int>>> q;
+    q.push({1, {0, 0}});
+    visited[0][0] = true;
+    while(!q.empty())
+    {
+        int count = q.front().first;
+        int x = q.front().second.first;
+        int y = q.front().second.second;
+        q.pop();
+        if (x == maps.size() - 1 && y == maps[0].size() - 1)
+        {
+            cout << count << endl;
+            answer = count;
+            break;
+        }
+        for(int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx < 0 || ny < 0 || nx >= maps.size() || ny >= maps[0].size())
+                continue;
+            else if(visited[nx][ny] || maps[nx][ny] == 0)
+                continue;
+            visited[nx][ny] = 1;
+            q.push({count + 1, {nx, ny}});
+        }
+    }
+    if(answer == 10001)
+        answer = -1;
+    return answer;
+}
