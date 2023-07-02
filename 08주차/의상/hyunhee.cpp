cf5cc503c6d07cf403a7995c3fc315db27f9744a
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-07-02 13:44:26 +0900

    Update hyunhee.cpp

--- 08주차/의상/hyunhee.cpp
+++ 08주차/의상/hyunhee.cpp
@@ -1,1 +1,18 @@
+#include <string>
+#include <map>
+#include <vector>
+#include <iostream>
+using namespace std;
 
+int solution(vector<vector<string>> clothes) {
+    map<string, int> mp;
+    for(int i = 0; i < clothes.size(); i++){
+        mp[clothes[i][1]]++;
+    }
+    int answer = 1;
+    for (auto i = mp.begin(); i != mp.end(); i++){ //map에서 요소 참조하는 방법
+        answer *= (i->second + 1);
+    }
+    answer -= 1;
+    return answer;
+}
