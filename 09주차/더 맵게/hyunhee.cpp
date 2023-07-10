267126e8285876fb68d4114ec916f0a2fa15a374
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-07-10 14:51:43 +0900

    Update hyunhee.cpp

--- 09주차/더 맵게/hyunhee.cpp
+++ 09주차/더 맵게/hyunhee.cpp
@@ -8,7 +8,6 @@ using namespace std;
 int solution(vector<int> scoville, int K) {
     int answer = 0;
     priority_queue<int, vector<int>, greater<int>> scov; //heap을 의미, 자동 오름차순정렬되는 queue (그냥 <int>면 내림차순 정렬한다. 복잡하지만 외워야함.)
-    sort(scoville.begin(), scoville.end());
     for(int i = 0; i < scoville.size(); i++)
         scov.push(scoville[i]);
     for(int i = 0; i < scoville.size(); i++)
