26a616648e2e397513862688c755cc6e1a4679ca
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-08-03 13:21:08 +0900

    Update hyunhee.cpp

--- 11주차/징검다리 건너기/hyunhee.cpp
+++ 11주차/징검다리 건너기/hyunhee.cpp
@@ -1,1 +1,28 @@
 //다른 알고리즘 어떤것이 가능할까
+#include <string>
+#include <vector>
+using namespace std;
+bool check(int n, const vector<int>& stones, int k) {
+    int cur = 0;
+    for (int i = 0; i < stones.size(); i++) {
+        if (stones[i] - n <= 0)
+            cur++;
+        else
+            cur = 0;
+        if (cur >= k)
+            return false;
+    }
+    return true;
+}
+int solution(vector<int> stones, int k) {
+    long long left = 1;
+    long long right = 200000000;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (check(mid, stones, k))
+            left = mid + 1;//최대를 구하고 싶으면 left를 옮겨라
+        else
+            right = mid - 1;//최소를 구하고 싶으면 right를 옮겨라
+    }
+    return left;//그럼 left가 정답일 것이다.
+}
