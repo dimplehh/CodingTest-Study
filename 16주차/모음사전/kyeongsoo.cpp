db01a59999ef481a7270eb60924bdd115b53b81d
Bright Kim <118050445+newtron-vania@users.noreply.github.com>
2023-09-04 23:44:25 +0900

    Create kyeongsoo.cpp

---
+++ 16주차/모음사전/kyeongsoo.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void dfs(string tmp, string word, int &result, int &answer){
+    vector<char> alpha = {'A', 'E', 'I', 'O', 'U'};
+    if(tmp.compare(word) == 0){
+        answer = result;
+        return;
+    }
+    if(tmp.size() < 5){
+        for(int i = 0; i<5; i++){
+            result++;
+            dfs(tmp + alpha[i], word, result, answer);
+        }
+    }
+    
+}
+
+int solution(string word) {
+    int answer = 0;
+    int result = 0;
+    string tmp = "";
+    dfs("", word, result, answer);
+    
+    return answer;
+}
