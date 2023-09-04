2bebb0a5caa0ae869f5ed895a0fdb1bec41d5d4d
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-09-04 22:48:48 +0900

    Update hyunhee.cpp

--- 16주차/기능개발/hyunhee.cpp
+++ 16주차/기능개발/hyunhee.cpp
@@ -1,1 +1,29 @@
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
 
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    stack<int> st;
+    int max = 0;
+    int temp;
+    for(int i = 0; i < progresses.size(); i++)
+    {
+        temp = (100 - progresses[i]) / speeds[i];
+        if(!((100 - progresses[i]) % speeds[i] == 0)) temp += 1;
+        if(i == 0) max = temp;
+        else
+        {
+            if(temp > max)
+            {
+                max = temp;
+                answer.push_back(st.size());
+                while(!st.empty()) st.pop();
+            }
+        }
+        st.push(temp);
+    }
+    answer.push_back(st.size());
+    return answer;
+}
