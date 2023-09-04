941146e7437dc8074fc578008e66c211747c2d48
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-09-04 16:18:43 +0900

    Update hyunhee.cpp

--- 16주차/옹알이/hyunhee.cpp
+++ 16주차/옹알이/hyunhee.cpp
@@ -1,1 +1,33 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
 
+int solution(vector<string> babbling) {
+    int answer = 0;
+    string lst[4] = {"aya", "ye", "woo", "ma"};
+    string temp = "";
+    int length;
+    for(auto babble : babbling)
+    {
+        length = 0;
+        temp = "";
+        for(int i = 0; i < babble.size() ; i++)// "ayaye"
+        {
+            for(int j = 0; j < 4; j ++)//lst[0] ~lst[3]
+            {
+                if(babble.substr(i, lst[j].size()) == lst[j]) 
+                {
+                    if(temp == babble.substr(i, lst[j].size()))
+                        break;
+                    length += lst[j].size();
+                    temp = lst[j];
+                    i += lst[j].size() - 1; // i + lst[j].size() 부터 다시 검토
+                }
+            }
+        }
+        if(length == babble.size())
+            answer++;
+    }
+    return answer;
+}
