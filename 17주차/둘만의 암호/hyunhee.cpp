a3459ec3416f368795d5369faef51b4b4313d9e7
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-09-11 23:06:58 +0900

    Update hyunhee.cpp

--- 17주차/둘만의 암호/hyunhee.cpp
+++ 17주차/둘만의 암호/hyunhee.cpp
@@ -12,7 +12,7 @@ string solution(string s, string skip, int index) {
         {
             s[i]++;
             if(s[i] > 'z') s[i] = s[i] + 'a' - 'z' - 1; //z보다 큰 s[i]를 바꿔주는 게 find보다 먼저가 되어야 함
-            if(skip.find(s[i]) == string::npos)j++;
+            if(skip.find(s[i]) == string::npos)j++; //skip에 없을 때만 j를 증가시켜줌
         }
         answer += s[i];
     }
