281213c3f6f3fb7531ff783e4c7987cb43b485e7
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-09-20 13:09:07 +0900

    Update hyunhee.cpp

--- 18주차/디펜스 게임/hyunhee.cpp
+++ 18주차/디펜스 게임/hyunhee.cpp
@@ -1,1 +1,24 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
 
+int solution(int n, int k, vector<int> enemy) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(int i = 0; i < enemy.size(); i++)
+    {
+        pq.push(enemy[i]);
+        if(i >= k)
+        {
+            int temp = pq.top();
+            pq.pop();
+            n -= temp;
+            if(n < 0)
+                break;
+        }
+        answer++;
+    }
+    return answer;
+}
