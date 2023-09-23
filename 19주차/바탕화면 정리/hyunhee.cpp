534d2056b10b09e150e3d956dc2ed8cf8fd553fd
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-09-23 16:24:52 +0900

    Update hyunhee.cpp

--- 19주차/바탕화면 정리/hyunhee.cpp
+++ 19주차/바탕화면 정리/hyunhee.cpp
@@ -1,1 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
 
+vector<int> solution(vector<string> wallpaper) {
+    vector<int> answer;
+    int lux = 50, luy = 50, rdx = 0, rdy = 0;
+    for(int j = 0; j < wallpaper.size(); j++)
+    {
+        for(int i = 0; i < wallpaper[j].size(); i++)
+        {
+            if(wallpaper[j][i] == '#')
+            {
+                if(luy > i) luy = i;
+                if(rdy <= i) rdy = i + 1;
+            }
+        }
+        if(wallpaper[j].find('#') != string::npos)
+        {
+            if(lux > j) lux = j;
+            if(rdx <= j) rdx = j + 1;
+        }
+    }
+    answer = {lux, luy, rdx, rdy};
+    return answer;
+}
