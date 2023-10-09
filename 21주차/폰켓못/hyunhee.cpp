7f2e0b4eb241f546c74753910da6170c708442b5
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-10-09 14:08:19 +0900

    Update hyunhee.cpp

--- 21주차/폰켓못/hyunhee.cpp
+++ 21주차/폰켓못/hyunhee.cpp
@@ -1,1 +1,18 @@
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
 
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    unordered_map<int, int> mp;
+    for(int i = 0; i <  nums.size(); i++) 
+        mp[nums[i]]++;
+    for(auto iter = mp.begin();iter != mp.end(); iter++)
+    {
+        if(iter->second >= 1) answer++;
+        if(answer == nums.size() / 2) break;
+    }
+    return answer;
+}
