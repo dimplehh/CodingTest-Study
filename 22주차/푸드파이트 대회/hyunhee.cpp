75871c168bec2d1713dd799a71903d69cbcb4841
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-10-15 11:27:54 +0900

    Update hyunhee.cpp

--- 22주차/푸드파이트 대회/hyunhee.cpp
+++ 22주차/푸드파이트 대회/hyunhee.cpp
@@ -1,1 +1,30 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
 
+string solution(vector<int> food) {
+    string answer = "";
+    stack<int> st;
+    for(int i = 1; i < food.size(); i++)
+    {
+        int temp = food[i] / 2;
+        if(temp > 0)
+        {
+            for (int j = 0; j < temp; j++)
+            {
+                answer += to_string(i);
+                st.push(i);
+            }
+        }
+    }
+    answer += '0';
+    while(!st.empty())
+    {
+        answer += to_string(st.top());
+        st.pop();
+    }
+    return answer;
+}
