7d610f8d7f4b5f6e8aaa8bdc40224b2384734f1f
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2023-10-24 13:28:52 +0900

    Update hyunhee.cpp

--- 23주차/튜플/hyunhee.cpp
+++ 23주차/튜플/hyunhee.cpp
@@ -1,1 +1,41 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
 
+bool cmp(pair<int,int> &a, pair<int, int> &b)
+{
+    if (a.second == b.second) return a.first > b.first;
+	return a.second > b.second;
+}
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    map<int, int> mp;
+    int i = 0;
+    int now = 0;
+    while(s[i])
+    {
+        if('0' <= s[i] && s[i] <= '9')
+        {
+            now = i;
+            for(int j = i; j < s.size(); j++)
+            {
+                if(s[j] == ',' || s[j] == '}')
+                {
+                    mp[stoi(s.substr(now, j))]++;
+                    i = j;
+                    break;
+                }
+            }
+        }
+        else i++;
+    }
+    vector<pair<int, int>>vec(mp.begin(), mp.end());
+    sort(vec.begin(), vec.end(), cmp);
+    for(auto num:vec)
+        answer.push_back(num.first);
+    return answer;
+}
